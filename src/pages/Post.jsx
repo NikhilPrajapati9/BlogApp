fb632602d832c9185f46fa0e5bbf301c72b1b424
Nikhil Prajapati <nikhilprajapati3003@gmail.com>
2025-04-08 18:11:56 +0530

    update postpage ui

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -37,35 +37,35 @@ export default function Post() {
   return post ? (
     <div className="py-8 bg-[#030712] text-white rounded-2xl my-5">
       <Container>
-        <div className="w-full flex justify-center mb-4 rounded-xl p-2">
+        {isAuthor && (
+          <div className="flex justify-end px-3 mb-3 w-ful">
+            <Link to={`/edit-post/${post.$id}`}>
+              <Button
+                bgColor=""
+                className="mr-3 items-center flex h-10 cursor-pointer border-2 border-green-500 "
+              >
+                Edit
+              </Button>
+            </Link>
+            <Button
+              className="h-10 items-center flex cursor-pointer border-2 border-red-500"
+              bgColor=""
+              onClick={deletePost}
+            >
+              Delete
+            </Button>
+          </div>
+        )}
+        <div className="w-full flex flex-col lg:flex-row mb-4 rounded-xl p-2">
           <img
             src={post.featuredImage}
             alt={post.title}
-            className="rounded-xl max-w-120 outline-none "
+            className="rounded-xl lg:w-120 outline-none"
           />
-          <div className="w-full mb-6 text-start px-5">
-            <h1 className="text-2xl font-bold">{post.title}</h1>
+          <div className="w-full mt-8 lg:mt-0 mb-6 text-start px-5">
+            <h1 className="text-2xl font-bold mb-3">{post.title}</h1>
             <div className="browser-css">{parse(post.content)}</div>
           </div>
-          {isAuthor && (
-            <div className="flex">
-              <Link to={`/edit-post/${post.$id}`}>
-                <Button
-                  bgColor=""
-                  className="mr-3 items-center flex h-10 cursor-pointer border-2 border-green-500 "
-                >
-                  Edit
-                </Button>
-              </Link>
-              <Button
-                className="h-10 items-center flex cursor-pointer border-2 border-red-500"
-                bgColor=""
-                onClick={deletePost}
-              >
-                Delete
-              </Button>
-            </div>
-          )}
         </div>
       </Container>
     </div>
